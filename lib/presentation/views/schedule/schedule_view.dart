deada8323d8ce078b7dcc93a2bda6f75788739fb
Elena G Blanco <elena.guzbla@gmail.com>
2025-01-03 22:23:04 +0100

    📑 Add tabs for scheduled visits

--- lib/presentation/views/schedule/schedule_view.dart
+++ lib/presentation/views/schedule/schedule_view.dart
@@ -14,29 +14,91 @@ class _SchedulingViewState extends State<SchedulingView> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-            body: SingleChildScrollView(
-      child: Column(
-        //Filters
-
-        // Appointments and schedule
-        children: [
-          Text('Schedule',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-          SectionHeader(title: 'Nearest visit'),
-          Column(spacing: 10, children: [
-            ...List.generate(nearest.length, (index) {
-              return VisitCard(visitScheduled: schedules[index]);
-            })
-          ]),
-          SectionHeader(title: 'Future visits'),
-          Column(children: [
-            ...List.generate(futures.length, (index) {
-              return VisitCard(visitScheduled: schedules[index]);
-            })
-          ]),
-        ],
-      ),
-    )));
+        child: Column(children: [
+      Text('Schedule',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+      Expanded(
+        child: DefaultTabController(
+          length: 3,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: TabBar(
+                  indicatorColor: Colors.purple,
+                  unselectedLabelColor: Colors.grey.withValues(alpha: .5),
+                  labelStyle: const TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  labelColor: Colors.white,
+                  indicator: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  tabs: [
+                    ...List.generate(
+                      tabs.length,
+                      (index) => Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Tab(
+                          text: tabs[index],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          // Active visits
+                          SectionHeader(title: 'Nearest visit'),
+                          Column(spacing: 10, children: [
+                            ...List.generate(nearest.length, (index) {
+                              return VisitCard(
+                                  visitScheduled: schedules[index]);
+                            })
+                          ]),
+                          SectionHeader(title: 'Future visits'),
+                          Column(children: [
+                            ...List.generate(futures.length, (index) {
+                              return VisitCard(
+                                  visitScheduled: schedules[index]);
+                            })
+                          ]),
+                        ],
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        "Completed",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    const Center(
+                      child: Text(
+                        "Canceled",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
+    ]));
   }
 }
